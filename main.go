fef226a9faa9dfddea95b3b71d39f1ad5a1dcac0
Brian Hechinger <wonko@4amlunch.net>
2017-04-07 09:24:03 -0400

    Testing to see if drone is alive.

--- main.go
+++ main.go
@@ -5,31 +5,68 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"time"
+	"errors"
+	"database/sql"
 )
 
-func main() {
-	doit()
+type DBTest interface {
+	Init(driver string) error
+	Ping(timeout int) error
 }
 
-func doit() {
-	db, err := sqlx.Open("postgres",
-		"user=myapp host=localhost password=mysecretpassword dbname=myapp sslmode=disable")
-	if err != nil {
-		fmt.Println("Error Opening!")
-	}
+type DBInfo struct {
+	Driver string
+	DSN string
+	Conn *sqlx.DB
+}
 
+func (db *DBInfo) Init(driver, DSN string) error {
+	var err error
+	db.Conn, err = sqlx.Open(driver, DSN)
+	if err != nil {
+		return errors.New("Error opening connection!")
+	}
+	return nil
+}
+
+func (db *DBInfo) Ping(timeout int) error {
 	for try := 0;; try++ {
-		fmt.Println(try)
-		if try > 100 {
-			fmt.Println("giving up")
-			break
+		if try > timeout {
+			return errors.New("giving up")
 		}
-		if err := db.Ping(); err != nil {
-			fmt.Printf("Error pinging db: %s\n", err)
-		} else {
-			fmt.Println("It works!")
-			break
+		if err := db.Conn.Ping(); err == nil {
+			return nil
 		}
 		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	var err error
+	var result sql.Result
+
+	db := DBInfo{}
+	err = db.Init("postgres", "user=myapp host=localhost password=mysecretpassword dbname=myapp sslmode=disable")
+	if err != nil {
+		fmt.Printf("db.Init failed: %s", err)
+	}
+
+	err = db.Ping(15)
+	if err != nil {
+		fmt.Printf("db.Ping failed: %s", err)
+	} else {
+		fmt.Println("Ping!")
+	}
+
+	schema := `CREATE TABLE place (
+		country text,
+		city text NULL,
+		telcode integer);`
+
+	result, err = db.Conn.Exec(schema)
+	if err == nil {
+		fmt.Printf("CREATE SCHEMA: %s", result)
+	} else {
+		fmt.Printf("ERROR :: CREATE SCHEMA: %s", err)
 	}
 }
