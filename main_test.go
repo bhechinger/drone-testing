0b0f89ec665226fb198afc4e0a23b14426da9183
Brian Hechinger <wonko@4amlunch.net>
2017-04-13 09:45:17 -0400

    Is coveralls eating my stdout?

--- main_test.go
+++ main_test.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestPostgres(t *testing.T) {
+	fmt.Println("Testing PostgreSQL")
 	doit("postgres")
 }
 
 func TestMysql(t *testing.T) {
+	fmt.Println("Testing MySQL")
 	doit("mysql")
 }
