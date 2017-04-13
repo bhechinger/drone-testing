bb543c401abc8abcd9067eee23594dbd48e9a8e0
Brian Hechinger <wonko@4amlunch.net>
2017-04-13 11:55:23 -0400

    I R SO SMRT

--- main_test.go
+++ main_test.go
@@ -6,19 +6,19 @@ import (
 )
 
 func TestPostgres(t *testing.T) {
-	fmt.Println("Testing PostgreSQL",
+	fmt.Println("Testing PostgreSQL")
+	doit("postgres",
 		"user=myapp host=localhost password=mysecretpassword dbname=myapp sslmode=disable")
-	doit("postgres")
 }
 
 func TestMysql(t *testing.T) {
-	fmt.Println("Testing MySQL",
+	fmt.Println("Testing MySQL")
+	doit("mysql",
 		"user=myapp host=localhost password=mysecretpassword dbname=myapp sslmode=disable")
-	doit("mysql")
 }
 
 func TestMysql(t *testing.T) {
-	fmt.Println("Testing Maria",
+	fmt.Println("Testing Maria")
+	doit("mysql",
 		"user=myapp host=localhost port=9876 password=mysecretpassword dbname=myapp sslmode=disable")
-	doit("mysql")
 }
