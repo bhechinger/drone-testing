87999bd358930afe952cc29edbfe364ab337b763
Brian Hechinger <wonko@4amlunch.net>
2017-04-13 12:54:15 -0400

    This is cool as hell, let's see if it works

--- main_test.go
+++ main_test.go
@@ -5,29 +5,11 @@ import (
 	"fmt"
 )
 
-func TestPostgres(t *testing.T) {
-	fmt.Println("Testing PostgreSQL")
+func TestDB(t *testing.T) {
+	fmt.Println("Testing DB")
 	err := doit("postgres",
 		"user=myapp host=localhost password=mysecretpassword dbname=myapp sslmode=disable")
 	if err != nil {
-		t.Errorf("PostgreSQL failed: %v", err)
-	}
-}
-
-func TestMysql(t *testing.T) {
-	fmt.Println("Testing MySQL")
-	err := doit("mysql",
-		"user=myapp host=localhost password=mysecretpassword dbname=myapp sslmode=disable")
-	if err != nil {
-		t.Errorf("MySQL failed: %v", err)
-	}
-}
-
-func TestMariadb(t *testing.T) {
-	fmt.Println("Testing Maria")
-	err := doit("mysql",
-		"user=myapp host=localhost port=9876 password=mysecretpassword dbname=myapp sslmode=disable")
-	if err != nil {
-		t.Errorf("MariaDB failed: %v", err)
+		t.Errorf("DB Test failed: %v", err)
 	}
 }
